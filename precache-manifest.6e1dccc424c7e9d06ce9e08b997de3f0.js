b8e31afd7659a81ee06c2d7a9637c9114edcfe39
alam1386 <alam1386@gmail.com>
2020-07-24 11:18:39 -0400

    Updates

---
+++ precache-manifest.6e1dccc424c7e9d06ce9e08b997de3f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b501287182e8c8e5399641c0ba69c9a5",
+    "url": "/github-jobs/index.html"
+  },
+  {
+    "revision": "b2471ca97edf81abc2a0",
+    "url": "/github-jobs/static/js/2.31040715.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/github-jobs/static/js/2.31040715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92b50c86b16a3f95b38e",
+    "url": "/github-jobs/static/js/main.f831a82d.chunk.js"
+  },
+  {
+    "revision": "7a765713f3b14e51360d",
+    "url": "/github-jobs/static/js/runtime-main.f452bb19.js"
+  }
+]);
